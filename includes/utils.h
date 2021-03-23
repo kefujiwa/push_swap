102cb71fcd22dde8792b48fe3a4afb890ae22451
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-03-24 01:59:21 +0900

    feat(is_valid_args.c): implement is_valid_args.c
    - is_valid_args.c 実装

---
+++ includes/utils.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/23 02:26:23 by kefujiwa          #+#    #+#             */
+/*   Updated: 2021/03/23 22:35:10 by kefujiwa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+/*
+** SELF-MADE HEADER FILES
+*/
+# include "libft.h"
+
+/*
+** EXTERNAL LIBRARIES
+*/
+# include <limits.h>
+# include <stdlib.h>
+
+/*
+** PROTOTYPE DECLARATION
+*/
+is_valid_args(char **argv);
+
+#endif
---
+++ srcs/utils/is_valid_args.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_valid_args.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/23 02:15:52 by kefujiwa          #+#    #+#             */
+/*   Updated: 2021/03/24 01:24:03 by kefujiwa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+
+static int	within_range(char *str, int is_negative)
+{
+	unsigned int	num;
+	unsigned int	max;
+	int				digit;
+
+	num = 0;
+	max = INT_MAX;
+	digit = 7;
+	if (is_negative)
+		digit = 8;
+	while (*str)
+	{
+		if (num < max / 10 || (num == max / 10 && *str - '0' <= digit))
+			num = num * 10 + (*str = '0');
+		else
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	is_numeric(char *str)
+{
+	int		is_negative;
+	char	*num;
+	int		ret;
+
+	is_negative = 0;
+	if (*str == '-' || *str == '+')
+	{
+		is_negative = (*str == '-' ? 1 : 0);
+		str++;
+	}
+	if (!*str)
+		return (0);
+	num = str;
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (0);
+		str++;
+	}
+	ret = within_range(num, is_negative);
+	return (ret);
+
+int	is_valid_args(char **argv)
+{
+	char	**tmp
+
+	argv++;
+	while (*argv)
+	{
+		if (!is_numeric(*argv));
+			return (0);
+		tmp = argv;
+		while (*tmp)
+		{
+			if (!ft_strcmp(*argv, *tmp))
+				return (0);
+			tmp++;
+		}
+		argv++;
+	}
+	return (1);
+}
