5dca7c7bddd03937beeecd5a78360b1bb15745c3
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-03-24 17:45:11 +0900

    feat(checker.c): implement checker.c
    - checker.c 実装

---
+++ includes/checker.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/23 02:27:56 by kefujiwa          #+#    #+#             */
+/*   Updated: 2021/03/24 17:43:51 by kefujiwa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHECKER_H
+# define CHECKER_H
+
+/*
+** SELF-MADE HEADER FILES
+*/
+# include "utils.h"
+
+/*
+** PROTOTYPE DECLARATION
+*/
+
+#endif
---
+++ srcs/checker/checker.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/23 02:08:30 by kefujiwa          #+#    #+#             */
+/*   Updated: 2021/03/24 17:31:07 by kefujiwa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker.h"
+
+static int	is_instruction(char *str)
+{
+	int			i;
+	const char	*instructions[] = {
+		"sa", "sb", "ss", "pa", "pb", "ra", "rb", "rr", "rra", "rrb", "rrr"};
+
+	while (instructions[i])
+	{
+		if (str && !ft_strcmp(instructions[i], str))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	init_list(t_list **a, t_list **b, t_list **inst, char **argv)
+{
+	char	*line;
+	int		ret;
+
+	*a = NULL;
+	*b = NULL;
+	*inst = NULL;
+	while (*argv)
+		ft_lstadd_back(a, ft_lstnew(ft_atoi(*argv++)));
+	while ((ret = get_next_line(0, &line)) > 0)
+	{
+		if (!is_instruction(line))
+			return (EXIT_FAILURE);
+		ft_lstadd_back(inst, ft_lstnew(line));
+	}
+	if (ret == -1)
+		return (EXIT_FAILURE);
+}
+
+int			main(int argc, char **argv)
+{
+	t_list	*a;
+	t_list	*b;
+	t_list	*inst;
+
+	if (argc == 1)
+		return (EXIT_SUCCESS);
+	if (!is_valid_args(argv))
+		return (put_error());
+	init_list(&a, &b, &inst, argv);
+	perform_instruction(&a, &b, inst);
+	check_result(&a, &b);
+	ft_lstclear(a, NULL);
+	ft_lstclear(b, NULL);
+	ft_lstclear(inst, free);
+	return (EXIT_SUCCESS);
+}
