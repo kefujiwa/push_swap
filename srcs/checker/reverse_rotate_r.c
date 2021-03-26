2653b3ec640850912bca54feac316c37b2be4866
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-03-26 13:37:45 +0900

    fix(reverse_rotate_r.c): fix infinite loop error
    - 最後から一個手前のリスト要素のnextメンバをNULLにする処理を追加することで、無限ループエラー解消

--- srcs/checker/reverse_rotate_r.c
+++ srcs/checker/reverse_rotate_r.c
@@ -6,7 +6,7 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 23:17:24 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/03/26 00:05:54 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/03/26 13:33:26 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,23 @@
 
 static void	reverse_rotate(t_list **lst)
 {
+	t_list	*last;
 	t_list	*tmp;
+	int		size;
 
-	tmp = ft_lstlast(*lst);
-	ft_lstadd_front(lst, tmp);
+	last = ft_lstlast(*lst);
+	tmp = *lst;
+	size = ft_lstsize(tmp) - 2;
+	while (size--)
+		tmp = tmp->next;
+	tmp->next = NULL;
+	ft_lstadd_front(lst, last);
 }
 
 void		reverse_rotate_r(t_list **lst1, t_list **lst2)
 {
-	if (lst1 && *lst1)
+	if (lst1 && *lst1 && (*lst1)->next)
 		reverse_rotate(lst1);
-	if (lst2 && *lst2)
+	if (lst2 && *lst2 && (*lst2)->next)
 		reverse_rotate(lst2);
 }
