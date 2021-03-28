f0a8d6f5897b167c8eec8f37351e1f9066c00174
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-03-29 01:22:24 +0900

    refactor(is_sorted.c): decrease amount of parameter to one; remove b->first existence check process
    - 引数の数を1つに変更
    - b->first存在チェックを削除
    - あくまで任意のスタックのソートチェック機能に変更

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 02:26:23 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/03/28 23:29:32 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/03/29 01:14:40 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ typedef struct	s_stack
 ** PROTOTYPE DECLARATION
 */
 int	build_stack(t_stack *a, t_stack *b, char **argv);
-int	is_sorted(t_stack *a, t_stack *b);
+int	is_sorted(t_stack *st);
 int	is_valid_args(char **argv);
 int	push(t_stack *dst, t_stack *src);
 int	put_error(void);
--- srcs/utils/is_sorted.c
+++ srcs/utils/is_sorted.c
@@ -6,24 +6,22 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/28 17:06:37 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/03/28 23:27:17 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/03/29 01:13:37 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
 
-int	is_sorted(t_stack *a, t_stack *b)
+int	is_sorted(t_stack *st)
 {
 	t_dlist	*head;
 
-	head = a->first;
-	while (head != a->last)
+	head = st->first;
+	while (head != st->last)
 	{
 		if ((int)head->content > (int)head->next->content)
 			return (INVALID);
 		head = head->next;
 	}
-	if (b->first)
-		return (INVALID);
 	return (VALID);
 }
