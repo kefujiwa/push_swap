987cd2bec0004553f8dc4f8e35bee17ef06419b3
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-04-07 01:50:33 +0900

    fix(ascii art): modify ascii art due to norminette
    - using multi-byte word causes norminette error
    - modify ascii art because of the reason above

--- srcs/checker/output_result.c
+++ srcs/checker/output_result.c
@@ -6,7 +6,7 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 23:33:53 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/04/03 19:56:48 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/04/07 01:10:10 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,46 @@
 
 static void	put_black_white_ko(void)
 {
-	ft_putendl_fd("██╗  ██╗ ██████╗ ", STDOUT_FILENO);
-	ft_putendl_fd("██║ ██╔╝██╔═══██╗", STDOUT_FILENO);
-	ft_putendl_fd("█████╔╝ ██║   ██║", STDOUT_FILENO);
-	ft_putendl_fd("██╔═██╗ ██║   ██║", STDOUT_FILENO);
-	ft_putendl_fd("██║  ██╗╚██████╔╝", STDOUT_FILENO);
-	ft_putendl_fd("╚═╝  ╚═╝ ╚═════╝ ", STDOUT_FILENO);
+	ft_putendl_fd(" ___  __    ________", STDOUT_FILENO);
+	ft_putendl_fd("|\\  \\|\\  \\ |\\   __  \\", STDOUT_FILENO);
+	ft_putendl_fd("\\ \\  \\/  /|\\ \\  \\|\\  \\", STDOUT_FILENO);
+	ft_putendl_fd(" \\ \\   ___  \\ \\  \\\\\\  \\", STDOUT_FILENO);
+	ft_putendl_fd("  \\ \\  \\\\ \\  \\ \\  \\\\\\  \\", STDOUT_FILENO);
+	ft_putendl_fd("   \\ \\__\\\\ \\__\\ \\_______\\", STDOUT_FILENO);
+	ft_putendl_fd("    \\|__| \\|__|\\|_______|", STDOUT_FILENO);
 }
 
 static void	put_colored_ko(void)
 {
-	ft_putendl_fd(RED"██╗  ██╗ ██████╗ "RESET, STDOUT_FILENO);
-	ft_putendl_fd(RED"██║ ██╔╝██╔═══██╗"RESET, STDOUT_FILENO);
-	ft_putendl_fd(RED"█████╔╝ ██║   ██║"RESET, STDOUT_FILENO);
-	ft_putendl_fd(RED"██╔═██╗ ██║   ██║"RESET, STDOUT_FILENO);
-	ft_putendl_fd(RED"██║  ██╗╚██████╔╝"RESET, STDOUT_FILENO);
-	ft_putendl_fd(RED"╚═╝  ╚═╝ ╚═════╝ "RESET, STDOUT_FILENO);
+	ft_putendl_fd(RED" ___  __    ________", STDOUT_FILENO);
+	ft_putendl_fd("|\\  \\|\\  \\ |\\   __  \\", STDOUT_FILENO);
+	ft_putendl_fd("\\ \\  \\/  /|\\ \\  \\|\\  \\", STDOUT_FILENO);
+	ft_putendl_fd(" \\ \\   ___  \\ \\  \\\\\\  \\", STDOUT_FILENO);
+	ft_putendl_fd("  \\ \\  \\\\ \\  \\ \\  \\\\\\  \\", STDOUT_FILENO);
+	ft_putendl_fd("   \\ \\__\\\\ \\__\\ \\_______\\", STDOUT_FILENO);
+	ft_putendl_fd("    \\|__| \\|__|\\|_______|"RESET, STDOUT_FILENO);
 }
 
 static void	put_black_white_ok(void)
 {
-	ft_putendl_fd(" ██████╗ ██╗  ██╗", STDOUT_FILENO);
-	ft_putendl_fd("██╔═══██╗██║ ██╔╝", STDOUT_FILENO);
-	ft_putendl_fd("██║   ██║█████╔╝ ", STDOUT_FILENO);
-	ft_putendl_fd("██║   ██║██╔═██╗ ", STDOUT_FILENO);
-	ft_putendl_fd("╚██████╔╝██║  ██╗", STDOUT_FILENO);
-	ft_putendl_fd(" ╚═════╝ ╚═╝  ╚═╝", STDOUT_FILENO);
+	ft_putendl_fd(" ________  ___  __", STDOUT_FILENO);
+	ft_putendl_fd("|\\   __  \\|\\  \\|\\  \\", STDOUT_FILENO);
+	ft_putendl_fd("\\ \\  \\|\\  \\ \\  \\/  /|_", STDOUT_FILENO);
+	ft_putendl_fd(" \\ \\  \\\\\\  \\ \\   ___  \\", STDOUT_FILENO);
+	ft_putendl_fd("  \\ \\  \\\\\\  \\ \\  \\\\ \\  \\", STDOUT_FILENO);
+	ft_putendl_fd("   \\ \\_______\\ \\__\\\\ \\__\\", STDOUT_FILENO);
+	ft_putendl_fd("    \\|_______|\\|__| \\|__|", STDOUT_FILENO);
 }
 
 static void	put_colored_ok(void)
 {
-	ft_putendl_fd(GREEN" ██████╗ ██╗  ██╗"RESET, STDOUT_FILENO);
-	ft_putendl_fd(GREEN"██╔═══██╗██║ ██╔╝"RESET, STDOUT_FILENO);
-	ft_putendl_fd(GREEN"██║   ██║█████╔╝ "RESET, STDOUT_FILENO);
-	ft_putendl_fd(GREEN"██║   ██║██╔═██╗ "RESET, STDOUT_FILENO);
-	ft_putendl_fd(GREEN"╚██████╔╝██║  ██╗"RESET, STDOUT_FILENO);
-	ft_putendl_fd(GREEN" ╚═════╝ ╚═╝  ╚═╝"RESET, STDOUT_FILENO);
+	ft_putendl_fd(GREEN" ________  ___  __", STDOUT_FILENO);
+	ft_putendl_fd("|\\   __  \\|\\  \\|\\  \\", STDOUT_FILENO);
+	ft_putendl_fd("\\ \\  \\|\\  \\ \\  \\/  /|_", STDOUT_FILENO);
+	ft_putendl_fd(" \\ \\  \\\\\\  \\ \\   ___  \\", STDOUT_FILENO);
+	ft_putendl_fd("  \\ \\  \\\\\\  \\ \\  \\\\ \\  \\", STDOUT_FILENO);
+	ft_putendl_fd("   \\ \\_______\\ \\__\\\\ \\__\\", STDOUT_FILENO);
+	ft_putendl_fd("    \\|_______|\\|__| \\|__|"RESET, STDOUT_FILENO);
 }
 
 void		output_result(t_stack *a, t_stack *b, int flag)
--- srcs/utils/exit_error.c
+++ srcs/utils/exit_error.c
@@ -6,7 +6,7 @@
 /*   By: kefujiwa <kefujiwa@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 01:56:00 by kefujiwa          #+#    #+#             */
-/*   Updated: 2021/04/06 15:57:03 by kefujiwa         ###   ########.fr       */
+/*   Updated: 2021/04/07 01:21:21 by kefujiwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,48 @@
 
 static void	put_black_white_err(void)
 {
-	ft_putendl_fd("███████╗██████╗ ██████╗  ██████╗ ██████╗ ", STDERR_FILENO);
-	ft_putendl_fd("██╔════╝██╔══██╗██╔══██╗██╔═══██╗██╔══██╗", STDERR_FILENO);
-	ft_putendl_fd("█████╗  ██████╔╝██████╔╝██║   ██║██████╔╝", STDERR_FILENO);
-	ft_putendl_fd("██╔══╝  ██╔══██╗██╔══██╗██║   ██║██╔══██╗", STDERR_FILENO);
-	ft_putendl_fd("███████╗██║  ██║██║  ██║╚██████╔╝██║  ██║", STDERR_FILENO);
-	ft_putendl_fd("╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝", STDERR_FILENO);
+	ft_putendl_fd(" _______   ________  ________  ________  ________"
+			, STDERR_FILENO);
+	ft_putendl_fd("|\\  ___ \\ |\\   __  \\|\\   __  \\|\\   __  \\|\\   __  \\"
+			, STDERR_FILENO);
+	ft_putstr_fd("\\ \\   __/|\\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\|\\  \\ \\  "
+			, STDERR_FILENO);
+	ft_putendl_fd("\\|\\  \\", STDERR_FILENO);
+	ft_putstr_fd(" \\ \\  \\_|/_\\ \\   _  _\\ \\   _  _\\ \\  \\\\\\  \\ \\   "
+			, STDERR_FILENO);
+	ft_putendl_fd("_  _\\", STDERR_FILENO);
+	ft_putstr_fd("  \\ \\  \\_|\\ \\ \\  \\\\  \\\\ \\  \\\\  \\\\ \\  \\\\\\  "
+			, STDERR_FILENO);
+	ft_putendl_fd("\\ \\  \\\\  \\|", STDERR_FILENO);
+	ft_putstr_fd("   \\ \\_______\\ \\__\\\\ _\\\\ \\__\\\\ _\\\\ \\_______\\ "
+			, STDERR_FILENO);
+	ft_putendl_fd("\\__\\\\ _\\", STDERR_FILENO);
+	ft_putstr_fd("    \\|_______|\\|__|\\|__|\\|__|\\|__|\\|_______|"
+			, STDERR_FILENO);
+	ft_putendl_fd("\\|__|\\|__|", STDERR_FILENO);
 }
 
 static void	put_colored_err(void)
 {
-	ft_putendl_fd(YELLOW"███████╗██████╗ ██████╗  ██████╗ ██████╗ "RESET,
-			STDERR_FILENO);
-	ft_putendl_fd(YELLOW"██╔════╝██╔══██╗██╔══██╗██╔═══██╗██╔══██╗"RESET,
-			STDERR_FILENO);
-	ft_putendl_fd(YELLOW"█████╗  ██████╔╝██████╔╝██║   ██║██████╔╝"RESET,
-			STDERR_FILENO);
-	ft_putendl_fd(YELLOW"██╔══╝  ██╔══██╗██╔══██╗██║   ██║██╔══██╗"RESET,
-			STDERR_FILENO);
-	ft_putendl_fd(YELLOW"███████╗██║  ██║██║  ██║╚██████╔╝██║  ██║"RESET,
-			STDERR_FILENO);
-	ft_putendl_fd(YELLOW"╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝"RESET,
-			STDERR_FILENO);
+	ft_putendl_fd(YELLOW" _______   ________  ________  ________  ________"
+			, STDERR_FILENO);
+	ft_putendl_fd("|\\  ___ \\ |\\   __  \\|\\   __  \\|\\   __  \\|\\   __  \\"
+			, STDERR_FILENO);
+	ft_putstr_fd("\\ \\   __/|\\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\|\\  \\ \\  "
+			, STDERR_FILENO);
+	ft_putendl_fd("\\|\\  \\", STDERR_FILENO);
+	ft_putstr_fd(" \\ \\  \\_|/_\\ \\   _  _\\ \\   _  _\\ \\  \\\\\\  \\ \\   "
+			, STDERR_FILENO);
+	ft_putendl_fd("_  _\\", STDERR_FILENO);
+	ft_putstr_fd("  \\ \\  \\_|\\ \\ \\  \\\\  \\\\ \\  \\\\  \\\\ \\  \\\\\\  "
+			, STDERR_FILENO);
+	ft_putendl_fd("\\ \\  \\\\  \\|", STDERR_FILENO);
+	ft_putstr_fd("   \\ \\_______\\ \\__\\\\ _\\\\ \\__\\\\ _\\\\ \\_______\\ "
+			, STDERR_FILENO);
+	ft_putendl_fd("\\__\\\\ _\\", STDERR_FILENO);
+	ft_putstr_fd("    \\|_______|\\|__|\\|__|\\|__|\\|__|\\|_______|"
+			, STDERR_FILENO);
+	ft_putendl_fd("\\|__|\\|__|"RESET, STDERR_FILENO);
 }
 
 void		exit_error(t_stack *a, t_stack *b, int flag)
